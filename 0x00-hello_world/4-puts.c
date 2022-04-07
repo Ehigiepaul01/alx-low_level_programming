3972d39f3c29fd20f6516c79b1baabb07e40a1f3
Ehigiepaul01 <paulehigie95@gmaol.com>
2022-04-07 03:13:49 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("\"Progrmmaing is like building a multilingual puzzle");
+return (0);
+}
