ad628156925e94cf534f391cfcb8f7570b8d9714
Ehigiepaul01 <paulehigie95@gmaol.com>
2022-04-07 03:23:37 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(voi)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
