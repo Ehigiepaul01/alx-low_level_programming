8a8aacbfad6d4f9c26f4331cf8fa7ab4b41f26ab
Ehigiepaul01 <paulehigie95@gmaol.com>
2022-04-08 15:37:54 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: Print lower and upper alphabet on the same line
+ * Return: 0
+ */
+int main(void)
+{
+int c;
+
+for (c = 0; c < 26; ++c)
+putchar('a' + c);
+for (c = 0; c < 26; ++c)
+putchar('A' + c);
+
+putchar('\n');
+
+return (0);
+}
