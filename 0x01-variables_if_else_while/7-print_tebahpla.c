016ee985a56b75d8c0a02ee97a95ed6eed403e93
Ehigiepaul01 <paulehigie95@gmaol.com>
2022-04-08 15:52:07 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: Print lowercase alphabet backwards
+ * Return: 0
+ */
+int main(void)
+{
+char c;
+
+for (c = 'z'; c >= 'a'; --c)
+putchar(c);
+
+putchar('\n');
+
+return (0);
+}
