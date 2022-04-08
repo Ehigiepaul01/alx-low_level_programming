1fcd98ea5bfa81ea162604abbf0dd13a473ca4bc
Ehigiepaul01 <paulehigie95@gmaol.com>
2022-04-08 15:27:09 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,7 +4,7 @@
 
 /**
  * main - entry point
- * 
+ *
  * Description: Prints a number and it's last digit along with whether its
  * last digit is 0, greater than 5, or less than 6 and not 0.
  * Return: 0
