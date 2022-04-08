8efff8ce684423e57fa3a484b0391a7de0929d46
Ehigiepaul01 <paulehigie95@gmaol.com>
2022-04-08 15:41:28 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: Print lowercase alphabet excluding e and q
+ * Return: 0
+ */
+int main(void)
+{
+char c;
+
+for (c = 'a'; c <= 'z'; ++c)
+{
+if (c != 'e' && c != 'q')
+putchar(c);
+}
+
+putchar('\n');
+
+return (0);
+}
