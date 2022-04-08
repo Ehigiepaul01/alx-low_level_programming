f8c4f1890bf4e27345fbed373cc4cbf5dd4fd874
Ehigiepaul01 <paulehigie95@gmaol.com>
2022-04-08 15:45:54 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: Print numbers 0 to 9 on one line
+ * Return: 0
+ */
+int main(void)
+{
+char i;
+
+for (i = '0'; i <= '9'; ++i)
+putchar(i);
+
+putchar('\n');
+
+return (0);
+}
