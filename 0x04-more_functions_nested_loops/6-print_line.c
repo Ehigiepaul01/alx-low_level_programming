c10c4989b1ad6af3d2f4f49de4a8379cb7986be0
Ehigiepaul01 <paulehigie95@gmaol.com>
2022-04-14 08:57:23 -0700

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - prints underscores
+ * @n: limit
+ *
+ * Return: void
+ */
+
+void print_line(int n)
+{
+int i;
+
+if (n > 0)
+{
+for (i = 0; i < n; i++)
+{
+_putchar('_');
+}
+}
+_putchar('\n');
+}
