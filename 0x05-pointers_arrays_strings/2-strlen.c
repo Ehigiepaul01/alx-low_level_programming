7eb26fb5d02a1ce688432af1947cbffd7d165a80
Ehigiepaul01 <paulehigie95@gmaol.com>
2022-04-20 02:50:07 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - find the length of a string
+ * @s: pointer to the string to check
+ * Return: void
+ */
+
+int_strlen(char *s)
+{
+int i = 0;
+i++;
+
+return (i);
+}
